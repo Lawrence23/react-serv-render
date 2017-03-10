5ce895a8474d3096bf2ef5a334377eb8814d8188
Lawrence <lawrence@qburst.com>
2017-03-10 15:17:45 +0530

    build script

--- routes/index.jsx
+++ routes/index.jsx
@@ -1,7 +1,7 @@
+var router = require('express').Router();
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
 var ReactRouter = require('react-router');
-var router = require('express').Router();
 
 router.get('*', function(request, response) {
 	var props = { title: 'Universal React App' };
